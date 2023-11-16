42fee699bdb7525f3950de55ca8f8018155461af
wifek <benabdallah.wifek@esprit.tn>
2023-11-16 16:25:03 +0100

    first commit

---
+++ src/ZooManagement.java
@@ -0,0 +1,11 @@
+public class ZooManagement {
+    int nbrCages = 20;
+    String zooName = "myZoo";
+
+    public static void main(String[] args) {
+        ZooManagement zm = new ZooManagement();
+        System.out.println(zm.zooName + " comporte " + zm.nbrCages);
+
+    }
+}
+}
